c796ad8cfb20b651b761ba026d814d2839b10204
Muhammad Sayyedain <120954497+SayyedainDev@users.noreply.github.com>
2026-09-13 21:39:44 +0500

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.py
@@ -0,0 +1,35 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Rotate the array to the right by k positions.
+        Modify nums in-place.
+        """
+
+        n = len(nums)
+
+        # In case k is greater than the length of the array
+        k = k % n
+
+        def reverse(left, right):
+            while left < right:
+                nums[left], nums[right] = nums[right], nums[left]
+                left += 1
+                right -= 1
+
+        # Step 1: Reverse the entire array
+        reverse(0, n - 1)
+
+        # Step 2: Reverse the first k elements
+        reverse(0, k - 1)
+
+        # Step 3: Reverse the remaining elements
+        reverse(k, n - 1)
+
+
+# -----------------------------
+# Test the code
+# -----------------------------
+
