8ebb220c8afb7a92fdbec4bf8d5e1ab0f5d6ce27
Muhammad Sayyedain <120954497+SayyedainDev@users.noreply.github.com>
2026-09-18 11:14:43 +0500

    Add: duplicate-integer - submission-15

---
+++ Data Structures & Algorithms/duplicate-integer/submission-15.py
@@ -0,0 +1,13 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        freq={}
+        for i in range(len(nums)):
+               if nums[i]  in freq:
+                    return True
+               freq[nums[i]]=nums[i]
+        return False
+          
+          
+            
+
+        
