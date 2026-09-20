9c8de6679a67b825185eb6d4db2fe33c2be8fea8
Muhammad Sayyedain <120954497+SayyedainDev@users.noreply.github.com>
2026-09-20 15:26:55 +0500

    Add: design-hashset - submission-5

---
+++ Data Structures & Algorithms/design-hashset/submission-5.py
@@ -0,0 +1,51 @@
+class ListNode:
+    def __init__(self,key):
+        self.key=key
+        self.next=None
+
+class MyHashSet:
+
+    def __init__(self):
+        self.map=[ListNode(0) for i in range(10**4)]
+    
+    def hash(self,key):
+        return key%len(self.map)
+      
+        
+
+    def add(self, key: int) -> None:
+        curr=self.map[self.hash(key)]
+        while curr.next:
+            if curr.next.key==key:
+                return
+            curr=curr.next
+        curr.next=ListNode(key)
+
+    
+    def remove(self, key: int) -> None:
+        curr=self.map[self.hash(key)]
+        while curr.next:
+            if curr.next.key==key:
+                curr.next=curr.next.next
+                return
+            curr=curr.next
+        
+       
+        
+
+        
+
+    def contains(self, key: int) -> bool:
+        curr=self.map[self.hash(key)]
+        while curr.next:
+            if curr.next.key==key:
+                return True
+            curr=curr.next
+        return False
+        
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
